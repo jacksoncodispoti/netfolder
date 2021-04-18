b37c39c5d4d4d83433ea211558e9128099935e28
Alerik <alerik@alerik.de>
2021-04-18 18:36:00 -0400

    Major re-organization

--- src/main.rs
+++ src/main.rs
@@ -1,60 +1,220 @@
 use clap::{App};
-use std::io::{self, Write};
 
-fn start_server() {
-
-}
-
-mod client {
-    use std::io::{self, Write};
+mod error {
     use std::error::Error;
-    use colour;
+    use std::fmt;
 
-    mod error {
-        use std::error::Error;
-        use std::fmt;
+    #[derive(Debug)]
+    pub struct ArgError {
+        details: String
+    }
 
-        #[derive(Debug)]
-        pub struct ArgError {
-            details: String
+    impl ArgError {
+        pub fn new(msg: &str) -> ArgError {
+            ArgError{details: msg.to_string()}
         }
+    }
 
-        impl ArgError {
-            pub fn new(msg: &str) -> ArgError {
-                ArgError{details: msg.to_string()}
-            }
+    impl fmt::Display for ArgError {
+        fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+            write!(f, "{}", self.details)
         }
+    }
 
-        impl fmt::Display for ArgError {
-            fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-                write!(f, "{}", self.details)
-            }
+    impl Error for ArgError {
+        fn description(&self) -> &str {
+            &self.details
         }
+    }
+}
+mod net {
+    use std::net::{self, TcpStream};
 
-        impl Error for ArgError {
-            fn description(&self) -> &str {
-                &self.details
+    pub const PACKET_SIZE: usize = 512;
+
+    #[derive(Debug)]
+    pub enum Code {
+        Unknown=0x0,
+        Upload=0x1,
+        Download=0x2,
+        Delete=0x3,
+        Dir=0x4,
+        Hello=0x5
+    }
+
+    impl Code {
+        pub fn from_u8(value: u8) -> Code {
+            match value {
+                0x0 => Code::Unknown,
+                0x1 => Code::Upload,
+                0x2 => Code::Download,
+                0x3 => Code::Delete,
+                0x4 => Code::Dir,
+                0x5 => Code::Hello,
+                _ => Code::Unknown
             }
         }
     }
 
+    pub fn parse_packet(packet: [u8; PACKET_SIZE]) -> Code {
+        if packet.len() < 1 {
+            Code::Unknown
+        }
+        else {
+            Code::from_u8(packet[0])
+        }
+    }
+
+    pub struct Connection {
+        pub name: String,
+        pub stream: Option<ConnectionStream>
+    }
+    impl Connection {
+        pub fn new(ip: net::IpAddr, port: u16) -> Connection {
+            Connection{name: String::from("Default name"), stream: Some(ConnectionStream::new(ip, port))}
+        }
+        pub fn default() -> Connection {
+            Connection{ name: String::from("no connection"), stream: None }
+        }
+    }
+
+    pub struct ConnectionStream {
+        stream: TcpStream
+    }
+
+    impl ConnectionStream {
+        pub fn new(ip: net::IpAddr, port: u16) -> ConnectionStream {
+            let stream = TcpStream::connect((ip, port));
+            ConnectionStream{stream: stream.unwrap()}
+        }
+    }
+}
+mod encoding {
+   use std::net::{TcpStream};
+   pub struct FileEncoder<'a> {
+        stream: &'a mut TcpStream
+   }
+
+   impl FileEncoder<'_> {
+       pub fn new<'a>(stream: &'a mut TcpStream) -> FileEncoder<'a> {
+            FileEncoder {stream: stream}
+       }
+       pub fn add_upload(&self) {
+
+       }
+
+       pub fn add_receive(&self) {
+
+       }
+       
+       pub fn add_send(&self) {
+
+       }
+   }
+}
+mod server {
+    use std::net::{TcpListener, TcpStream, IpAddr, SocketAddr, Ipv4Addr};
+    use crate::encoding::FileEncoder;
+    use crate::net::{self, Code};
+
     struct Connection {
-        name: String
+        stream: TcpStream,
     }
 
     impl Connection {
-        fn new() -> Connection {
-            Connection{name: String::from("no connection")}
+        fn new(stream: TcpStream) -> Connection {
+            //let encoder = FileEncoder::new(&mut stream);
+            Connection { stream: stream }
+        }
+
+        fn handle(&mut self) {
+            let mut encoder = FileEncoder::new(&mut self.stream);
+            let mut buf = [0; net::PACKET_SIZE ];
+            loop {
+                match self.stream.peek(&mut buf) {
+                    Ok(size) => {
+                        let code = net::parse_packet(buf);
+                    },
+                    Err(_e) => {}
+                }
+            }
+        }
+
+        fn handle_command(&mut self, encoder: FileEncoder, command: Code) {
+            match command {
+                Code::Upload => { encoder.add_receive(); },
+                Code::Download => { encoder.add_send(); },
+                Code::Delete => {},
+                Code::Dir => {},
+                Code::Hello => {},
+                Code::Unknown => { println!("Unknown command!"); }
+                _ => { println!("Unknown command!"); }
+            }
         }
     }
 
-    fn client_prompt(connection: &Connection) {
+    pub struct NetFolderListener {
+        name: String,
+        listener: TcpListener
+    }
+
+    impl NetFolderListener {
+        pub fn new(name: &str, ip: IpAddr, port: u16) -> NetFolderListener {
+            let addr = SocketAddr::from((ip, port));
+            NetFolderListener{ name: String::from(name), listener: TcpListener::bind(addr).unwrap() }
+        }
+
+        pub fn connection_loop(&self) {
+            for stream in self.listener.incoming() {
+                match stream {
+                    Ok(stream) => {
+                        println!("Accepted connection from {}", stream.local_addr().unwrap());
+                        let mut connection = Connection::new(stream);
+                        connection.handle();
+                    }
+                    Err(e) => {
+                        println!("Error accepting incoming connection: {}", e);
+                    }
+                };
+            }
+        }
+
+    }
+
+    pub fn start_server() {
+        let ip = IpAddr::V4(Ipv4Addr::new(0, 0, 0, 0));
+        let port = 3219;
+        let listener = NetFolderListener::new("TheBlackPearl", ip, port);
+
+        listener.connection_loop();
+    }
+
+}
+mod client {
+    use std::io::{self, Write};
+    use crate::net;
+    use std::error::Error;
+    use crate::error;
+    use colour;
+
+
+    fn client_prompt(connection: &net::Connection) {
         print!("({}) > ", connection.name);
         io::stdout().flush().unwrap();
     }
 
-    fn connect(args: Vec<&str>) -> Result<(), Box<dyn Error>> {
+    fn connect(args: Vec<&str>, connection: &mut net::Connection) -> Result<(), Box<dyn Error>> {
         if args.len() == 2 {
+            let ip: std::net::IpAddr = args[0].parse().unwrap();
+            let port: u16 = args[1].parse().unwrap();
+
+            *connection = net::Connection::new(ip, port);
+            match &mut connection.stream {
+                Some(stream) => {
+                },
+                None => {}
+            };
+            
             Ok(()) 
         }
         else {
@@ -95,9 +255,9 @@ mod client {
         }
     }
 
-    fn run_command(command: &str, args: Vec<&str>) -> Result<(), Box<dyn Error>> {
+    fn run_command(connection: &mut net::Connection, command: &str, args: Vec<&str>) -> Result<(), Box<dyn Error>> {
         match command {
-            "connect" => { connect(args) },
+            "connect" => { connect(args, connection) },
             "upload" => { upload(args) },
             "download" => { download(args) },
             "delete" => { delete(args) },
@@ -106,7 +266,7 @@ mod client {
         }
     }
 
-    fn parse_command(command: &String) -> (String, Vec<&str>) {
+    fn parse_command<'a>(command: &'a String, connection: &mut net::Connection) -> (String, Vec<&'a str>) {
         let split = command.trim().split(" ").collect::<Vec<&str>>();
 
         if split.len() == 0 {
@@ -126,7 +286,7 @@ mod client {
     }
 
     fn client_shell() {
-        let mut connection = Connection::new();
+        let mut connection = net::Connection::default();
 
         loop {
             client_prompt(&connection);
@@ -135,8 +295,8 @@ mod client {
                 .read_line(&mut line)
                 .expect("Failed to read line");
 
-            let (command, args) = parse_command(&line);
-            match run_command(&command, args) {
+            let (command, args) = parse_command(&line, &mut connection);
+            match run_command(&mut connection, &command, args) {
                 Ok(()) => {},
                 Err(e)  => { colour::red_ln!("{:?}", e)}
             }
@@ -164,7 +324,7 @@ fn main() {
         .get_matches();
 
     if let Some(ref matches) = matches.subcommand_matches("server") {
-        start_server();
+        server::start_server();
         println!("Running the server");
     }
     else if let Some(ref matchef) = matches.subcommand_matches("client") {
